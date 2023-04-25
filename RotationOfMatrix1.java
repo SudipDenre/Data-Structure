b4afedc068687a33052e2eea32f136eac4bcd9e5
Sudip Denre <sudipdenre4@gmail.com>
2023-04-25 22:41:20 +0530

    Modified code added

--- RotationOfMatrix1.java
+++ RotationOfMatrix1.java
@@ -1,20 +1,18 @@
 import java.util.Arrays;
 // Brute Force Approach
+// Time Complexity = O(n^2)
+// Space Complexity = O(n^2)
 
 public class RotationOfMatrix1 {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][] a = new int[m][n];
-        int x = n-1;
-        for(int i=0;i<m;i++){
+        int n = arr.length;
+        int[][] a = new int[n][n];
+        
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(x>=0){
-                    a[j][x] = arr[i][j];
+                    a[j][n-1-i] = arr[i][j];
                 }
-            }
-            x--;
         }
         for(var v : a){
             System.out.println(Arrays.toString(v));
